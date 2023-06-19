e0a232f6d489fd41d1c8ff666f2eb2f95fb0320e
dshritama <dshritama@gmail.com>
2023-06-19 17:53:07 +0530

    first commit

---
+++ patient/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import PatientManagement from './components/patient';
+
+function App() {
+  return (
+    <div className="App">
+      <PatientManagement/>
+    </div>
+  );
+}
+
+export default App;
